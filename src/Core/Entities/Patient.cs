1ae46bfd3866fc1b4fb890eb5afa664c7c44b29f
Ryan Celeslie <ryan.celeslie@outlook.com>
2023-04-15 20:32:31 -0400

    Solution and project setup, also created project entity

---
+++ src/Core/Entities/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities;
+
+public enum Gender
+{
+    Male,
+    Female,
+    NonBinary,
+    Unknown
+}
+
+public class Patient : BaseEntity
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public DateTime DateOfBirth { get; private set; }
+    public Gender Gender { get; private set; }
+
+    public Patient(string firstName, string lastName, DateTime dateOfBirth, Gender gender)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+        Gender = gender;
+    }
+}
+
