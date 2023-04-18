f4ba9c31f26eff15e5f35e37b706da22455e35d3
Ryan Celeslie <ryan.celeslie@outlook.com>
2023-04-17 23:26:32 -0400

    Setup the infrastructure portions and implementation in the API project

--- src/Core/Interfaces/IRepository.cs
+++ src/Core/Interfaces/IRepository.cs
@@ -10,7 +10,12 @@ public interface IRepository<T> where T : class, IAggregateRoot
 {
     Task<T> AddAsync(T entity);
     Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(T entity);
+    Task DeleteAsync(T entity);
     Task<T> GetByIdAsync<Tid>(Tid id) where Tid : notnull;
+
+    // I prefer to use different collection types for these kind of properties as the usages
+    // of the collection should determine which type to use. .NET collections are notorious
+    // with people misusing them or overlooking performance issues. They quickly become a
+    // bottleneck for applications
     Task<List<T>> GetAllAsync();
 }
---
+++ src/Infrastructure/Data/Repository.cs
@@ -0,0 +1,51 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data;
+
+public class Repository<T> : IRepository<T> where T : BaseEntity, IAggregateRoot
+{
+    private readonly PatientRecordsContext _context;
+
+    public Repository(PatientRecordsContext context)
+    {
+        _context = context;
+    }
+    public async Task<T> AddAsync(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<T> GetByIdAsync<Tid>(Tid id) where Tid : notnull
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+}
