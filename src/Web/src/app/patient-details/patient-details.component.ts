9c8e9a9f99ee65f74968f302c42a604ad7cd78d7
Ryan Celeslie <ryan.celeslie@outlook.com>
2023-04-27 14:55:26 -0400

    Added edit validation

--- src/Web/src/app/patient-details/patient-details.component.ts
+++ src/Web/src/app/patient-details/patient-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 // Material Imports
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
@@ -12,11 +13,25 @@ import { Patient } from 'src/models/patient.model';
   styleUrls: ['./patient-details.component.scss']
 })
 export class PatientDetailsComponent {
+  public patientForm: FormGroup<any>;
+
   constructor(
     public dialogRef: MatDialogRef<PatientDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Patient) {}
+    @Inject(MAT_DIALOG_DATA) public data: Patient) {
+      this.patientForm = new FormGroup({
+        fristName: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        dateOfBirth: new FormControl(new Date()),
+        gender: new FormControl('', [Validators.required, Validators.maxLength(2)])
+      });
+
+    }
 
   onCancelClick(): void {
     this.dialogRef.close();
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.patientForm.controls[controlName].hasError(errorName);
   }
 }
