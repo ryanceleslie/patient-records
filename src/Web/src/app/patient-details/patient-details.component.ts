556e19289cbf8105dc09609988ec3bb05b1351ea
Ryan Celeslie <ryan.celeslie@outlook.com>
2023-04-27 15:33:42 -0400

    Removed unnecessary code

--- src/Web/src/app/patient-details/patient-details.component.ts
+++ src/Web/src/app/patient-details/patient-details.component.ts
@@ -13,25 +13,11 @@ import { Patient } from 'src/models/patient.model';
   styleUrls: ['./patient-details.component.scss']
 })
 export class PatientDetailsComponent {
-  public patientForm: FormGroup<any>;
-
   constructor(
     public dialogRef: MatDialogRef<PatientDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Patient) {
-      this.patientForm = new FormGroup({
-        fristName: new FormControl('', [Validators.required]),
-        lastName: new FormControl('', [Validators.required]),
-        dateOfBirth: new FormControl(new Date()),
-        gender: new FormControl('', [Validators.required, Validators.maxLength(2)])
-      });
-
-    }
+    @Inject(MAT_DIALOG_DATA) public data: Patient) { }
 
   onCancelClick(): void {
     this.dialogRef.close();
-  }
-
-  public hasError = (controlName: string, errorName: string) => {
-    return this.patientForm.controls[controlName].hasError(errorName);
   }
 }
