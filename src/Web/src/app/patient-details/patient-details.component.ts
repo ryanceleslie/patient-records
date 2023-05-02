c3a5d3154181c24dc600ae5665d6161c51b94c15
Ryan Celeslie <ryan.celeslie@outlook.com>
2023-05-02 14:44:59 -0400

    Added datepicker control and fixed the validate on submit, it was not actually working

--- src/Web/src/app/patient-details/patient-details.component.ts
+++ src/Web/src/app/patient-details/patient-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 // Material Imports
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
@@ -13,9 +13,28 @@ import { Patient } from 'src/models/patient.model';
   styleUrls: ['./patient-details.component.scss']
 })
 export class PatientDetailsComponent {
-  constructor(
-    public dialogRef: MatDialogRef<PatientDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Patient) { }
+  public patientDetailsEditForm: FormGroup;
+
+  constructor(fb: FormBuilder, public dialogRef: MatDialogRef<PatientDetailsComponent>, @Inject(MAT_DIALOG_DATA) public patient: Patient) 
+  {
+    this.patientDetailsEditForm = fb.group({
+      firstName: new FormControl(patient.firstName, [Validators.required]),
+      lastName: new FormControl(patient.lastName, [Validators.required]),
+      dateOfBirth: new FormControl(patient.dateOfBirth, [Validators.required]),
+      gender: new FormControl(patient.gender, [Validators.required, Validators.maxLength(2)])
+    });
+  }
+
+  public hasError = (controlName: string, errorName: string) =>{
+    return this.patientDetailsEditForm.controls[controlName].hasError(errorName);
+  }
+
+  onUpdateClick() {
+    const {value, valid} = this.patientDetailsEditForm;
+    if(valid){
+      this.dialogRef.close(value);
+    }
+}
 
   onCancelClick(): void {
     this.dialogRef.close();
